f879a46d215c5439e3ecd6e62a8461252ff46a4e
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-25 10:13:31 -0500

    Fixed the PortfolioContainer file to properly export it and import it to other files. Tabs in navbar are now working so that the different pages no longer show up all at once.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,18 @@
 // import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar({currentPage, handlePageChange}) {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="../App">
+        <a
+          class="navbar-brand"
+          href="#home"
+          onClick={() => handlePageChange("About")}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
           Matt Reynolds
         </a>
         <button
@@ -22,22 +29,38 @@ export default function Navbar() {
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./About">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#About"
+                onClick={() => handlePageChange("About")}
+                // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                className={currentPage === "About" ? "nav-link active" : "nav-link"}
+              >
                 About Me
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./Projects">
+              <a class="nav-link active" aria-current="page" href="#Projects"
+          onClick={() => handlePageChange('Projects')}
+          // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>
                 Projects
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./Contact">
-                Contact
+              <a class="nav-link active" aria-current="page" href="#Contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                Contact Me
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./Resume">
+              <a class="nav-link active" aria-current="page" href="#Resume"
+          onClick={() => handlePageChange('Resume')}
+          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
                 Resume
               </a>
             </li>
