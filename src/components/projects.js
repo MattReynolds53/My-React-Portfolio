e934ea70ffbd79b8380ab75d77e71ce4e664ad8c
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-27 17:15:49 -0600

    Got links to projects on projects page to load the proper external pages.

--- src/components/projects.js
+++ src/components/projects.js
@@ -24,72 +24,96 @@ export default function Projects() {
     <div class="card" style={styles.cardWidth}>
       <img src={project1} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">My Travel Advisor</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <div class="card-body">
-        <a href="#github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/WishTravelP2" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
   
     <div class="card" style={styles.cardWidth}>
       <img src={project2} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">MyMusic</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <div class="card-body">
-        <a href="github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/ProjectforHackers" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
 
     <div class="card" style={styles.cardWidth}>
       <img src={project3} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Tech Blog</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <div class="card-body">
-        <a href="github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/My-Tech-Blog" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
 
     <div class="card" style={styles.cardWidth}>
       <img src={project4} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Weather Dashboard</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <div class="card-body">
-        <a href="github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/Forecast-Dashboard" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
 
     <div class="card" style={styles.cardWidth}>
       <img src={project5} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Note Taker</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <div class="card-body">
-        <a href="github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/Everyday-Note-Taker" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
 
     <div class="card" style={styles.cardWidth}>
       <img src={project6} class="card-img-top" alt="Project 1"></img>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">Coding Team Generator</h5>
+        <p class="card-text">Utilize the command line to efficiently display your coding team with their most relevant information.</p>
       </div>
       <div class="card-body">
-        <a href="github.com" target="_blank" class="card-link">GitHub Repository</a>
+        <p>
+        <a href="https://github.com/MattReynolds53/Coding-Team-Generator" target="_blank" class="card-link">GitHub Repository</a>
+        </p>
+        <p>
         <a href="google.com" target="_blank" class="card-link">Deployed Application</a>
+        </p>
       </div>
     </div>
   </div>
